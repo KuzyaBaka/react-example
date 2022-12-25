6506f67e02e0a3eef4a289ef73154fb6748d6dea
KuzyaBaka <youryz58@gmail.com>
2022-12-25 19:42:45 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Button from "./components/Button/Button";
+
+function App() {
+  return (
+    <>
+      <Button />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default class Button extends React.Component {
+  // Отвечает за отрисовку
+  render() {
+    return <button>Home</button>;
+  }
+}
